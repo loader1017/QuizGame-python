33cc63dcecc73c1d967ef724dd0a077104cc4071
Sungeun <dohc2069@naver.com>
2026-04-06 16:37:30 +0900

    파일생성 및 퀴즈클래스 생성

--- quizmain.py
+++ quizmain.py
@@ -1,7 +1,11 @@
 Class Menu:
+    def __init__(selection):
+        selection.options = ["Add Quiz", "Take Quiz", "Exit"]
 
-
-
+    def display(selection):
+        print("Menu:")
+        for idx, option in enumerate(selection.options, 1):
+            print(f"{idx}. {option}")
 
 
 
