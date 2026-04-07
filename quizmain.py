df5a2a9929766bca7168e65866269d78d6b40584
Sungeun <dohc2069@naver.com>
2026-04-07 11:15:57 +0900

    퀴즈점수브렌치생성및스코어json생성

--- quizmain.py
+++ quizmain.py
@@ -28,13 +28,25 @@ menu = Menu(
 )
 
 QUIZ_FILE = "data.json"
+LSCORE = "score.json"
 
+# 퀴즈 데이터 불러오기 함수
 def load_quizzes():
     if not os.path.exists(QUIZ_FILE):
         return []
-    with open(QUIZ_FILE, "r", encoding="utf-8") as f:
-        data = json.load(f)
+    with open(QUIZ_FILE, "r", encoding="utf-8") as file:
+        data = json.load(file)
     return [Quiz(q["question"], q["answer"]) for q in data]
+
+#점수 데이터 불러오기 및 저장 함수
+def save_score(score, total):
+    with open(LSCORE, "w", encoding="utf-8") as file:
+        json.dump({"last_score": f"{score}/{total}"}, file)
+
+def load_score():
+    with open(LSCORE, "r", encoding="utf-8") as file:
+        data = json.load(file)
+    return data["last_score"]
 
 quiz_list = load_quizzes()  
 
@@ -43,7 +55,7 @@ if menu.startquiz:
     print("퀴즈를 시작합니다! O 또는 X로 답해주세요.")
     print("=" * 40)
 
-    score = 0 #초기화
+    score = 0 #시작스코어초기화
     for q in quiz_list:
         print(f"Q: {q.question}")
         user_input = input("A (O/X): ").strip().upper()  # .upper() 소문자로 입력해도 대문자로 변환, .strip() 답변에 딸려오는 공백 제거
@@ -58,20 +70,22 @@ if menu.startquiz:
 
     print(f"퀴즈 종료. 총점: {score}/{len(quiz_list)}")
 
+    save_score(score, len(quiz_list))  # 점수 저장
+
 #2. 퀴즈 추가
 elif menu.addquiz:
     print("퀴즈 추가 기능은 아직 구현되지 않았습니다.")
 
 #3. 퀴즈 목록
 elif menu.quizlist:
-    print("======== 퀴즈 목록 =======")
+    print("======== 등록 된 퀴즈 목록 =======")
     for i, q in enumerate(quiz_list, 1):
         print(f"{i}. {q.question} (정답: {q.answer})")
     print("===========================")
 
 #4. 점수 확인
 elif menu.scorecheck:
-    print(f"마지막으로 푼 퀴즈의 점수는 입니다.")
+    print(f"마지막으로 푼 퀴즈의 점수는 {load_score()}입니다.")
 
 #5. 종료
 elif menu.quit:
